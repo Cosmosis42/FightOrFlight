62551557aab258b92762bf538ffa51131e4a8691
Fenchurch Lunquawl <cameronryan@eastlink.ca>
2018-05-04 21:41:59 -0300

    basic movement

--- FightOrFlight/Assets/Scripts/BirdController.cs
+++ FightOrFlight/Assets/Scripts/BirdController.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction
+{
+    LEFT, RIGHT
+}
+
 public class BirdController : MonoBehaviour {
     private Rigidbody2D rb2D;
+    private Direction facing = Direction.RIGHT;
     public float flapStren = 10;
     public float moveSpeed = 10;
 
@@ -13,22 +19,24 @@ public class BirdController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
 
         // Pressing space will flap your wings, giving you upward thrust
         if (Input.GetKeyDown("space"))
         {
             rb2D.AddForce(transform.up * flapStren);
         }
-        
-        // Pressing the right or left buttons will move you in that direction
-        if (Input.GetKeyDown("D")) // Right
-        {
-            rb2D.
-        }
-        else if (Input.GetKeyDown("A")) // Left
-        {
 
+        //Pressing the right or left buttons will move you in that direction
+        if (Input.GetKey("d")) // Right
+        {
+            facing = Direction.RIGHT;
+            rb2D.AddForce(transform.right * moveSpeed);
+        }
+        else if (Input.GetKey("a")) // Left
+        {
+            facing = Direction.LEFT;
+            rb2D.AddForce(transform.right * -moveSpeed);
         }
     }
 }
