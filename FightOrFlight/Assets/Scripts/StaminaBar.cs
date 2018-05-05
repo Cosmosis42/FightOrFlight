e7cf821069900d0b682bdb97a3bc7928f4e5b0ca
BeyondMeta <sophie.twardus@gmail.com>
2018-05-05 10:59:32 -0300

    Added Stamina Bar file

---
+++ FightOrFlight/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaBar : MonoBehaviour {
+
+    int staminaBar1;
+    int staminaBar2;
+    int lives1;
+    int lives2;
+
+    public bool flightMode;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lives1 == 0)
+        {
+            Winner(2);
+        }
+        if (lives2 == 0)
+        {
+            Winner(1);
+        }
+
+
+        if (!flightMode)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+
+    void Winner(int i)
+    {
+
+    }
+
+}
