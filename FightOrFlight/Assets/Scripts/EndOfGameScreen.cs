bc1612919f67c190a6f61780594f36967e543065
MrBjarmastic <bammbeggason69@gmail.com>
2018-05-06 18:55:08 +0000

    created end game screen.

---
+++ FightOrFlight/Assets/Scripts/EndOfGameScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+	public class EndOfGameScreen : MonoBehaviour
+	{
+		public Button Continue;
+		public Button Exit;
+		public Text WinnerText;
+
+		public void Initialize(string winner)
+		{
+			Continue.Select();
+			WinnerText.text = winner;
+		}
+
+		public void EndGame()
+		{
+			Application.Quit();
+		}
+
+		public void RestartGame()
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
