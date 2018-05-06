3b192f590feab37e09c93e2537ecbafdfa03cf4a
MrBjarmastic <bammbeggason69@gmail.com>
2018-05-06 01:43:18 +0000

    PowerUps are now a thing. fixed animation bug

--- FightOrFlight/Assets/Scripts/HpPowerUp.cs
+++ FightOrFlight/Assets/Scripts/HpPowerUp.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
-public class HpPowerUp : APowerUp
+public class HpPowerUp : MonoBehaviour
 {
+	public PowerUpSpawner Spawner;
+
 	public int Value;
-	public override void Apply(Player player)
+
+	public void Apply(Player player)
 	{
 		player.AddHp(Value);
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			var playerController = other.gameObject.GetComponent<BirdController>();
+
+			Apply(playerController.player);
+
+			Destroy(gameObject);
+		}
 	}
 }
